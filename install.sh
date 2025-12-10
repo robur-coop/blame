1df6a922521bffc13c18186b938f2d31728ae465
Calascibetta Romain <romain.calascibetta@gmail.com>
2025-12-10 14:07:21 +0100

    Finally, intall blame.hvt

--- install.sh
+++ install.sh
@@ -2,4 +2,9 @@
 
 dune describe location --context solo5 --no-print-directory ./main.exe &> unikernel.path
 UNIKERNEL=$(cat unikernel.path)
+echo " STRIP $UNIKERNEL"
 strip $UNIKERNEL -o blame.hvt
+cat >blame.install<<EOF
+bin: [
+  "blame.hvt"
+]
