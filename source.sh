513ed6bb40aa531f7d3a366f0cd1101c3535831a
Calascibetta Romain <romain.calascibetta@gmail.com>
2026-03-16 17:21:27 +0100

    Add our fork of Zarith for the cross-compilation

--- source.sh
+++ source.sh
@@ -25,3 +25,4 @@
 [ ! -d "vendors/prettym" ] && opam source prettym --dir vendors/prettym
 [ ! -d "vendors/utcp" ] && opam source utcp --dir vendors/utcp
 [ ! -d "vendors/bancos" ] && opam source bancos --dir vendors/bancos
+[ ! -d "vendors/zarith" ] && opam source zarith --dir vendors/zarith
